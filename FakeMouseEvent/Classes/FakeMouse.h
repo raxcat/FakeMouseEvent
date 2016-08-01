a9074b6a9430c7104ccea11bd1ab20e84613d31c
raxcat <raxcat@gmail.com>
2016-08-01 17:03:57 +0800

    first commit for FakeMouse

--- FakeMouseEvent/Classes/FakeMouse.h
+++ FakeMouseEvent/Classes/FakeMouse.h
@@ -10,4 +10,25 @@
 
 @interface FakeMouse : NSObject
 
+// Method to post mouse move event use input point
++(void)postRelativeMouseMove:(NSPoint)delta;
+// Method to post mouse drag event use input point
++(void)postRelativeMouseDrag:(NSPoint)delta;
+
++(void) postDownOnCurrentPoint;
++(void) postMouseUpOnCurrentPoint;
+
+// Method to post mouse down event use input point
+// IOKit Coordinate System
++(void)postMouseDown:(NSPoint)currentPoint;
+// Method to post mouse up event use input point
+// IOKit Coordinate System
++(void)postMouseUp:(NSPoint)currentPoint;
+// Method to post mouse drag event use input point
+// IOKit Coordinate System
++(void)postMouseDrag:(NSPoint)currentPoint;
+
+
++(io_connect_t)serviceConnection;
+
 @end
--- FakeMouseEvent/Classes/FakeMouse.m
+++ FakeMouseEvent/Classes/FakeMouse.m
@@ -9,6 +9,245 @@
 #import "FakeMouse.h"
 @import IOKit;
 
+static io_connect_t _serviceConnection = MACH_PORT_NULL;
+
+/**
+ Point in CoreGraphics Coordinate System(Zero point ↙︎).
+ */
+static NSPoint currentPoint(){
+    NSPoint mouseLoc;
+    mouseLoc = [NSEvent mouseLocation]; //get current mouse position
+    NSLog(@"Mouse location(Zero Point ↙︎): %f %f", mouseLoc.x, mouseLoc.y);
+    return mouseLoc;
+}
+
+/**
+ Point in IOKit Coordinate System(Zero point ↖︎).
+ */
+static NSPoint currentPointFlipped(){
+    NSPoint point = [NSEvent mouseLocation];
+    point = NSMakePoint(point.x, NSHeight([NSScreen mainScreen].frame) - point.y);
+    NSLog(@"Mouse location(Zero Point ↖︎): %f %f", point.x, point.y);
+    return point;
+}
+
 @implementation FakeMouse
+
++ (io_connect_t)serviceConnection
+{
+    kern_return_t kr;
+    mach_port_t masterPort, service, iter;
+    if (_serviceConnection == MACH_PORT_NULL) {
+        // get master post
+        kr = IOMasterPort(MACH_PORT_NULL, &masterPort);
+        if (kr == KERN_SUCCESS && masterPort) {
+            // get matching service
+            kr = IOServiceGetMatchingServices(masterPort, IOServiceMatching(kIOHIDSystemClass), &iter);
+            if (kr == KERN_SUCCESS) {
+                service = IOIteratorNext(iter);
+                // get service connection for later use
+                kr = IOServiceOpen(service, mach_task_self(), kIOHIDParamConnectType, &_serviceConnection);
+                IOObjectRelease(service);
+                IOObjectRelease(iter);
+                NSLog(@"%d", _serviceConnection);
+            }
+        }
+        if (masterPort) {
+            IOObjectRelease(masterPort);
+        }
+        if (kr != KERN_SUCCESS) {
+            _serviceConnection = MACH_PORT_NULL;
+        }
+    }
+    return _serviceConnection;
+}
+
+// Method to post mouse move event use input point
++ (void)postRelativeMouseMove:(NSPoint)delta
+{
+    NSPoint current = currentPointFlipped();
+    IOGPoint locPoint;
+    locPoint.x = (SInt16)(current.x);
+    locPoint.y = (SInt16)(current.y);
+    
+    // create mouse move event for input point
+    NXEvent mouseEvent = {NX_MOUSEMOVED, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouseMove.dx = (SInt32)delta.x;
+    mouseEvent.data.mouseMove.dy = (SInt32)delta.y;
+    mouseEvent.data.mouseMove.subx = 0;
+    mouseEvent.data.mouseMove.suby = 0;
+    mouseEvent.data.mouseMove.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse move event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_MOUSEMOVED, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetRelativeCursorPosition);
+    }
+}
+
+// Method to post mouse drag event use input point
++ (void)postRelativeMouseDrag:(NSPoint)delta
+{
+    NSPoint current = currentPointFlipped();
+    current.x += delta.x;
+    current.y += delta.y;
+    [self postMouseDrag:current];
+    
+    //    IOGPoint locPoint;
+    //    locPoint.x = (SInt16)(current.x);
+    //    locPoint.y = (SInt16)(current.y);
+    //    // create mouse drag event for input point
+    //    NXEvent mouseEvent = {NX_LMOUSEDRAGGED, {0, 0}, 0, -1, 0};
+    //    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    //    mouseEvent.data.mouse.subx = delta.x;
+    //    mouseEvent.data.mouse.suby = delta.y;
+    //    mouseEvent.data.mouse.eventNum = 1;
+    //    mouseEvent.data.mouse.click = 1;
+    //    mouseEvent.data.mouse.pressure = 128;
+    //    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    //    // post mouse drag event
+    //    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    //    if (serviceConnection != MACH_PORT_NULL) {
+    //        kern_return_t kr;
+    //        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEDRAGGED, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetRelativeCursorPosition);
+    //#ifdef __DEBUG__
+    //        NSLog(@"Mouse Drag %ld", kr);
+    //#endif
+    //    }
+}
+
++(void) postDownOnCurrentPoint{
+    NSPoint current = currentPointFlipped();
+    IOGPoint locPoint;
+    locPoint.x = (SInt16)(current.x);
+    locPoint.y = (SInt16)(current.y);
+    // create mouse down event for input point
+    NXEvent mouseEvent = {NX_LMOUSEDOWN, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouse.subx = 0;
+    mouseEvent.data.mouse.suby = 0;
+    mouseEvent.data.mouse.eventNum = 1;
+    mouseEvent.data.mouse.click = 1;
+    mouseEvent.data.mouse.pressure = 128;
+    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse down event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEDOWN, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetCursorPosition);
+#ifdef __DEBUG_
+        NSLog(@"Nelson - Mouse Down %ld", kr);
+#endif
+    }
+}
+
++(void) postMouseUpOnCurrentPoint{
+    NSPoint current = currentPointFlipped();
+    IOGPoint locPoint;
+    locPoint.x = (SInt16)(current.x);
+    locPoint.y = (SInt16)(current.y);
+    // create mouse up event for input point
+    NXEvent mouseEvent = {NX_LMOUSEUP, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouse.subx = 0;
+    mouseEvent.data.mouse.suby = 0;
+    mouseEvent.data.mouse.eventNum = 1;
+    mouseEvent.data.mouse.click = 0;
+    mouseEvent.data.mouse.pressure = 128;
+    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse up event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEUP, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetCursorPosition);
+#ifdef __DEBUG_
+        NSLog(@"Nelson - Mouse Up %ld", kr);
+#endif
+    }
+}
+
+
+// Method to post mouse down event use input point
+// IOKit Coordinate System
++(void)postMouseDown:(NSPoint)currentPoint
+{
+    IOGPoint locPoint;
+    locPoint.x = currentPoint.x;
+    locPoint.y = currentPoint.y;
+    // create mouse down event for input point
+    NXEvent mouseEvent = {NX_LMOUSEDOWN, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouse.subx = 0;
+    mouseEvent.data.mouse.suby = 0;
+    mouseEvent.data.mouse.eventNum = 1;
+    mouseEvent.data.mouse.click = 1;
+    //    mouseEvent.data.mouse.pressure = 128;
+    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse down event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEDOWN, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetCursorPosition);
+#ifdef __DEBUG_
+        NSLog(@"Nelson - Mouse Down %ld", kr);
+#endif
+    }
+}
+
+// Method to post mouse up event use input point
+// IOKit Coordinate System
++(void)postMouseUp:(NSPoint)currentPoint
+{
+    IOGPoint locPoint;
+    locPoint.x = currentPoint.x;
+    locPoint.y = currentPoint.y;
+    // create mouse up event for input point
+    NXEvent mouseEvent = {NX_LMOUSEUP, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouse.subx = 0;
+    mouseEvent.data.mouse.suby = 0;
+    mouseEvent.data.mouse.eventNum = 1;
+    mouseEvent.data.mouse.click = 0;
+    mouseEvent.data.mouse.pressure = 128;
+    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse up event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEUP, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetCursorPosition);
+#ifdef __DEBUG_
+        NSLog(@"Nelson - Mouse Up %ld", kr);
+#endif
+    }
+}
+
+// Method to post mouse drag event use input point
+// IOKit Coordinate System
++(void)postMouseDrag:(NSPoint)currentPoint
+{
+    IOGPoint locPoint;
+    locPoint.x = currentPoint.x;
+    locPoint.y = currentPoint.y;
+    // create mouse drag event for input point
+    NXEvent mouseEvent = {NX_LMOUSEDRAGGED, {0, 0}, 0, -1, 0};
+    memset(&(mouseEvent.data), 0 , sizeof(mouseEvent.data));
+    mouseEvent.data.mouse.subx = 0;
+    mouseEvent.data.mouse.suby = 0;
+    mouseEvent.data.mouse.eventNum = 1;
+    mouseEvent.data.mouse.click = 1;
+    mouseEvent.data.mouse.pressure = 128;
+    mouseEvent.data.mouse.subType = NX_SUBTYPE_DEFAULT;
+    // post mouse drag event
+    io_connect_t serviceConnection = [FakeMouse serviceConnection];
+    if (serviceConnection != MACH_PORT_NULL) {
+        kern_return_t kr;
+        kr = IOHIDPostEvent(serviceConnection, NX_LMOUSEDRAGGED, locPoint, &mouseEvent.data, kNXEventDataVersion, 0, kIOHIDSetCursorPosition);
+#ifdef __DEBUG__
+        NSLog(@"Mouse Drag %ld", kr);
+#endif
+    }
+}
+
 
 @end
